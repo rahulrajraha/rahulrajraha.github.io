2136156a1294715d06bb48d8fda3995b35b66882
rahulrajraha <37178279+rahulrajraha@users.noreply.github.com>
2020-01-09 11:07:16 +0530

    Delete index.php

--- css/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$url = 'Location: '.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'];
-	if(strcmp($_SERVER['REQUEST_METHOD'],'POST')!=0){
-		session_destroy();
-		header($url);
-		exit;
-	}
-?>
